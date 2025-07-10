10941dd12083a5ca7f712e54759b077138812a8b
Kenlark <kenzokerachi@hotmail.fr>
2025-07-10 12:31:03 +0200

    fix: update socket URL to use the correct environment for deployment

--- src/socket.ts
+++ src/socket.ts
@@ -5,6 +5,6 @@ const socketUrl =
   (window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000"
-    : "https://backend-websocket-chat.fly.dev/");
+    : "https://backend-websocket-chat.onrender.com/");
 
 export const socket = io(socketUrl);
